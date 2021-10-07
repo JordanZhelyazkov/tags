90c4ab1f77856171d7e79aa748b77d8e49a9e0f0
Jordan <jordanasenovzhelyazkov@gmail.com>
2021-10-07 16:13:41 +0300

    fix

--- src/Tags.js
+++ src/Tags.js
@@ -2,7 +2,7 @@ import { render } from "react-dom";
 
 
 const Tags = () => {
-    const arrStrings = ['boomdev', 'task', 'tags', 'react'];
+    const arrStrings = ['boomdotdev', 'task', 'tags', 'react'];
     const arrStrList = arrStrings.map(tag => <li className="tag">#{tag}</li>);
          return <div className="tags">{arrStrList}</div>;
 }
